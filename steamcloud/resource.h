ebf5705649d5b1531db0b40c862180df27c4ad03
Jan Hodouš <rhscz@post.cz>
2021-04-26 07:21:49 +0200

    Change name to steam cloud

---
+++ steamcloud/resource.h
@@ -0,0 +1,36 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ vygeneroval soubor zahrnutí.
+// Použito steamcloud.rc
+//
+#define IDD_STEAMCLOUD_DIALOG          101
+#define IDR_ICON                        128
+#define ID_MENU_MINIMIZE                129
+#define IDD_DIALOG1                     130
+#define IDC_TRAYEN                      1000
+#define IDC_MINEN                       1001
+#define IDC_LISTFILES                   1002
+#define IDC_CONNECT                     1003
+#define IDC_APPID                       1004
+#define IDC_DELETE                      1005
+#define IDC_UPLOAD                      1006
+#define IDC_DIRUPLOAD                   1007
+#define IDC_DOWNLOAD                    1008
+#define IDC_EXIT                        1009
+#define IDC_REFRESH                     1010
+#define IDC_RICHEDIT21                  1011
+#define IDC_QUOTA                       1012
+#define IDC_DISCONNECT                  1013
+#define ID_MENU_MIN                     32771
+#define ID_MENU_MAX                     32772
+#define ID_MENU_EXIT                    32773
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_SYMED_VALUE           102
+#endif
+#endif
---
+++ steamcloud/steamcloud.cpp
@@ -0,0 +1,103 @@
+#include "pch.h"
+#include "framework.h"
+#include "steamcloud.h"
+#include "steamcloudDlg.h"
+#include <tchar.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CsteamcloudApp
+
+BEGIN_MESSAGE_MAP(CsteamcloudApp, CWinApp)
+	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+CsteamcloudApp::CsteamcloudApp()
+{
+	
+}
+
+CsteamcloudApp theApp;
+BOOL CsteamcloudApp::InitInstance()
+{
+	AfxInitRichEdit2();
+	CWinApp::InitInstance();
+	//SetErrorMode(0); //only debug
+	/*if (LoadLibraryA("libssl-3.dll") == NULL) //not important for crypto
+	{
+		errormes = GetLastError();
+		error++;
+	}
+	/*if (LoadLibraryA("libcrypto-3.dll") == NULL) //
+	{
+		//errormes = GetLastError(); //for debug
+		error++;
+	}
+	if (error > 0)
+	{
+		exit(-5);
+	}*/
+	//SetDlgItemTextW(IDC_COMBO1, L"MD5");
+	
+	//SetRegistryKey(_T("Local AppWizard-Generated Applications")); //not needed
+
+/*	CsteamcloudApp::m_pMainWnd->ShowWindow(SW_SHOW);
+	if (m_pDialog != NULL)
+	{
+		BOOL ret = m_pDialog->Create(IDD_HASHER2_DIALOG);
+
+		if (!ret)   //Create failed.
+		{
+			AfxMessageBox(_T("Error creating Dialog"));
+			delete m_pDialog;
+		}
+		else {
+			m_pDialog->ShowWindow(SW_SHOW);
+		}
+		
+	}
+	*/
+	
+	CsteamcloudDlg dlg;
+	m_pMainWnd = &dlg;
+	//DWORD resultxy;
+	INT_PTR nResponse = dlg.DoModal();
+	//resultxy = GetLastError();
+	//if (dlg.Create(IDD_HASHER2_DIALOG, 0) == 0)
+	//{
+		//exit(-1);
+	//}
+	if (nResponse == IDOK)
+	{
+		// TODO: Place code here to handle when the dialog is
+		//  dismissed with OK
+	}
+	else if (nResponse == IDCANCEL)
+	{
+		// TODO: Place code here to handle when the dialog is
+		//  dismissed with Cancel
+	}
+	else if (nResponse == -1)
+	{
+		TRACE(traceAppMsg, 0, "Warning: dialog creation failed, so application is terminating unexpectedly.\n");
+	}
+	else if (nResponse == IDABORT)
+	{
+		TRACE(traceAppMsg, 0, "Warning: dialog creation failed, so application is terminating unexpectedly2.\n");
+	}
+
+
+#if !defined(_AFXDLL) && !defined(_AFX_NO_MFC_CONTROLS_IN_DIALOGS)
+	ControlBarCleanUp();
+#endif
+
+	return FALSE;
+}
+int CsteamcloudApp::ExitInstance()
+{
+	CsteamcloudDlg abc;
+	Shell_NotifyIcon(NIM_DELETE, &abc.m_nidIconData);
+	return 0;
+}
